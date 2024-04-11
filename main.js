af00dd84e31ab9ae6e72e83611ae34a783c00f35
majid ali <mianmajid577@gmail.com>
2024-04-11 12:55:56 +0500

    Adding number guessing game

---
+++ main.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+console.log("\n\t WelCome to my Number guessing Game\n");
+const RandomNumber = Math.floor(Math.random() * 6);
+const answer = await inquirer.prompt([
+    {
+        name: "UserGuessedNumber",
+        type: "number",
+        message: "Enter ur num within 1~6"
+    }
+]);
+if (answer.UserGuessedNumber === RandomNumber) {
+    console.log("you win");
+}
+else {
+    console.log("you loose");
+}
